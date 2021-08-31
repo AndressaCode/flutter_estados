8989ca2bb5bd893bce80b2a9593b2e204b63a1a5
Andressa Souza <amw.andressa@gmail.com>
2021-08-31 10:50:42 -0300

    Ajustes finais do dashboard

--- lib/screens/transferencia/ultimas.dart
+++ lib/screens/transferencia/ultimas.dart
@@ -17,23 +17,35 @@ class UltimasTransferencias extends StatelessWidget {
             fontSize: 20.0,
           ),
         ),
-        Consumer<Transferencias>(
-          builder: (context, transferencias, child) {
-            return ListView.builder(
-                padding: EdgeInsets.all(8.0),
-              itemCount: 2,
+        Consumer<Transferencias>(builder: (context, transferencias, child) {
+          final _ultimasTransferencias =
+              transferencias.transferencias.reversed.toList();
+          final _quantidade = transferencias.transferencias.length;
+          int tamanho = 2;
+
+          // Se a quantidade for zero, não precisa verificar os outros 'ifs'
+          // Por isso essa verificação vem antes das outras
+          if(_quantidade == 0){
+            return SemTransferenciaCadastrada();
+          }
+
+          if (_quantidade < 2) {
+            tamanho = _quantidade;
+          }
+
+          return ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              itemCount: tamanho,
               shrinkWrap: true,
               itemBuilder: (context, indice) {
-                  return ItemTransferencia(transferencias.transferencias[indice]);
-              }
-            );
-          }
-        ),
+                return ItemTransferencia(_ultimasTransferencias[indice]);
+              });
+        }),
         ElevatedButton(
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
           ),
-          child: Text('Transferências'),
+          child: Text('Ver todas transferências'),
           onPressed: () {
             Navigator.push(context, MaterialPageRoute(builder: (context) {
               return ListaTransferencias();
@@ -41,6 +53,21 @@ class UltimasTransferencias extends StatelessWidget {
           },
         ),
       ],
+    );
+  }
+}
+
+class SemTransferenciaCadastrada extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(40.0),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text(
+          'Você ainda não cadastrou nenhuma transferência',
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
